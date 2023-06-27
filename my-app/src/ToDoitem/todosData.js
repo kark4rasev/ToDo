5bd0f87f082fadff397ccdf31da12cfab2c2a7eb
kark4rasev <91518057+kark4rasev@users.noreply.github.com>
2023-06-27 15:08:23 +0300

    Create todosData.js

---
+++ my-app/src/ToDoitem/todosData.js
@@ -0,0 +1,31 @@
+// фейковый джейсон из которого получаем информацию файл с данными 
+
+const todosData = [  // внутри массива наши задачи 
+    {
+        id: 0,
+        description: 'сходить в магазин',
+        completed: false, // заполнена задача или нет 
+    },
+    {
+        id: 1,
+        description: 'сварить борщ',
+        completed: false, // заполнена задача или нет 
+    },
+    {
+        id: 2,
+        description: 'помыть потолок',
+        completed: false, // заполнена задача или нет 
+    },
+    {
+        id: 3,
+        description: 'подмести стены',
+        completed: false, // заполнена задача или нет 
+    },
+    {
+        id: 4,
+        description: 'поморосить',
+        completed: false, // заполнена задача или нет 
+    }
+];
+
+export default todosData;
